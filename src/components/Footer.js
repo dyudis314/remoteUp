08ee71f0ea3ab3200ed59092840a951af21f98f4
Dylan Cole <dyudis314@gmail.com>
2021-01-16 00:30:58 -0500

    Footer edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,36 +10,30 @@ const Footer = () => {
         <Row>
         {/* Column 1 */}
           <Col>
-            <h4>Thicc Memes Inc</h4>
+            <h4></h4>
               <ul className="list-unstyled">
-                <li>123-456-7891</li>
-                <li>Moscow, Russa</li>
-                <li>555 Johnson Drive</li>
+              <li><a href="https://github.com/dyudis314/remoteUp" target="_blank">Contribute</a></li>
+              <li><a href="https://www.producthunt.com/posts/remote-jobs-api" target="_blank">Remote OK API</a></li>               
               </ul>
           </Col>
         {/* Column 2 */}
         <Col>
-            <h4>Dank Memes Inc</h4>
-              <ul className="list-unstyled">
-                <li>123-456-7891</li>
-                <li>Moscow, Russa</li>
-                <li>555 Johnson Drive</li>
-              </ul>
+
           </Col>
         {/* Column 3 */}
         <Col>
-            <h4>Yup Memes Inc</h4>
+            <h4></h4>
               <ul className="list-unstyled">
-                <li>123-456-7891</li>
-                <li>Moscow, Russa</li>
-                <li>555 Johnson Drive</li>
+                <li><a href="https://www.dylanyudis.com/" target="_blank">Dylan Yudis | Work</a></li>
+                <li><a href="https://twitter.com/dylanyudis" target="_blank">Twitter</a></li>
+                <li><a href="https://github.com/dyudis314" target="_blank">GitHub</a></li>
               </ul>
           </Col>
         </Row>
         <hr></hr>
         <Row>
           <p className="col-sm">
-            &copy;{new Date().getFullYear()} Dylan Yudis | All Rights Reserved.
+            &copy; {new Date().getFullYear()} Dylan Yudis | All Rights Reserved.
           </p>
         </Row>
       </div>     
